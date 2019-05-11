8241f94b3a2fa5e6597e64e9b06b6dbe7f072716
Michael N. Rubinstein <michael@polyblog.com>
2019-05-11 15:12:57 -0700

    Finished Section 5: replaced App function with App class

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => {
-    window.navigator.geolocation.getCurrentPosition(
-        (position) => console.log(position), // Success callback
-        (err) => console.log(err) // Failure callback
-    );
-    
-    return <div>Hi there!</div>;
+class App extends React.Component {
+    render() {
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => console.log(position), // Success callback
+            (err) => console.log(err) // Failure callback
+        );
+
+        return <div>Lattitude: </div>;
+    };
 };
 
 ReactDOM.render(
