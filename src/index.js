18706cc01d39054ebe728d6c202baf89d5a96ed2
Michael N. Rubinstein <michael@polyblog.com>
2019-05-12 14:47:13 -0700

    Finished Lesson 68 - added Loading Spinner

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner'; // hurm
 
 class App extends React.Component {
     // THIS IS THE ONLY TIME we do direct assignment to state!
@@ -24,7 +25,7 @@ class App extends React.Component {
             return <SeasonDisplay lat={this.state.lat} />;
         };
         
-        return <div>Loading...</div>;
+        return <Spinner />;
     };
 };
 
